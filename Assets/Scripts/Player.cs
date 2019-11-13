87d1f609f74eab58f3652aeaad33aca457af607f
Russell Sapienza <russap667@gmail.com>
2019-11-13 12:12:02 -0500

    New update for testing
    New version. The other versions are old and not updating

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    public int floor;
+    public int PlayerLocation;
+    public string[] Inventory = new string[10];
+    public int[] Decision; 
+
+    public void SavePlayer ()
+    {
+        SaveSystem.SavePlayerData(this);
+    }
+
+    public void LoadPlayer1 ()
+    {
+        PlayerData data = SaveSystem.LoadPlayerData();
+        floor = data.floor;
+        for (int i = 0; i < data.Inventory.Length; i++)
+        {
+            Inventory[i] = data.Inventory[i];
+        }
+        for (int i = 0; i < data.Decision.Length; i++)
+        {
+            Decision[i] = data.Decision[i];
+        }
+    }
+    public void PickUp(string NewItem)
+    {
+        string[] ItemArray = Inventory;
+
+        int i = 0;
+        while (i < 10)
+        {
+            string temp = ItemArray[i];
+            if (temp == "")
+            {
+
+                ItemArray[i] = NewItem;
+                break;
+            }
+            if (temp[temp.Length - 1] == 'S')
+            {
+                temp = temp.Substring(0, temp.Length - 1);
+            }
+            if (NewItem == temp.Substring(2) || NewItem == temp)
+            {
+
+                if (temp[0] >= 48 && temp[0] < 58)
+                {
+                    int temp2 = Int32.Parse(temp[0].ToString());
+                    temp2++;
+                    ItemArray[i] = (temp2.ToString() + ' ' + temp.Substring(2) + "S");
+                }
+                else
+                {
+                    ItemArray[i] = ("2 " + temp + "S");
+                    
+                }
+            }
+            i++;
+        }
+        
+       
+            Inventory = ItemArray;
+        
+    }
+}
---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UserInterface : MonoBehaviour
+{
+    
+    
+
+
+
+
+    public void ForwardDirection(Player p)
+    {
+        switch (p.PlayerLocation)
+        {
+            case 0: case 2:
+                p.PlayerLocation++;
+                break;
+            case 1:
+                p.PlayerLocation += 3;
+                break;
+            case 3:
+                p.PlayerLocation += 4;
+                break;
+        }
+        
+    }
+    public void LeftDirection(Player p)
+    {
+        switch (p.PlayerLocation)
+        {
+            case 1:
+                p.PlayerLocation++;
+                break;
+            case 4:
+                p.PlayerLocation += 4;
+                break;
+            case 3:
+                p.PlayerLocation += 2;
+                break;
+            case 9:
+                p.PlayerLocation -= 5;
+                break;
+            case 6:
+                p.PlayerLocation -= 3;
+                break;
+        }
+
+    }
+    public void RightDirection(Player p)
+    {
+        switch (p.PlayerLocation)
+        {
+            
+            case 4:
+                p.PlayerLocation += 5;
+                break;
+            case 3:
+                p.PlayerLocation += 3;
+                break;
+            case 2:
+                p.PlayerLocation--;
+                break;
+            case 8:
+                p.PlayerLocation -= 4;
+                break;
+            case 5:
+                p.PlayerLocation -= 2;
+                break;
+        }
+
+    }
+    public void BackDirection(Player p)
+    {
+        switch (p.PlayerLocation)
+        {
+
+            case 1:
+            case 3:
+                p.PlayerLocation--;
+                break;
+            case 4:
+                p.PlayerLocation -= 3;
+                break;
+            case 7:
+                p.PlayerLocation -= 4;
+                break;
+        }
+
+    }
+    public void PauseButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    
+}
